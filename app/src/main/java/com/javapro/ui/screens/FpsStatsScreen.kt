ff840abb43978a0969243736250f8cc078526e89
gojoyoFo <javapro15912@gmail.com>
2026-05-08 07:33:44 +0700

    a

--- app/src/main/java/com/javapro/ui/screens/FpsStatsScreen.kt
+++ app/src/main/java/com/javapro/ui/screens/FpsStatsScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurMaskFilter
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.*
@@ -26,7 +25,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
